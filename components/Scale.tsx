5779e8cc55d17927a2291df7d7beedf6672c33a4
Mark Alaniz <markalaniz000@gmail.com>
2022-09-05 16:41:38 -0500

    Use js fiddle iframe and work on passing data

--- components/Scale.tsx
+++ components/Scale.tsx
@@ -1,41 +1,35 @@
 import React, { useEffect } from "react";
-import { Factory, EasyScore, System } from "vexflow";
-// import { VexTab, Artist, Vex } from "vextab";
-const vextab = require('vextab');
 
 type Props = {
   scaleKey: string;
   scaleType: string;
 };
 
-const Scale = (props: Props) => {
+const Scale = ({ scaleKey, scaleType }: Props) => {
   useEffect(() => {
-    const outputNode = document.getElementById("output");
-    console.log('log VexTab', vextab);
-    // const Renderer = Vex.Flow.Renderer;
+    console.log("log scaleKey", scaleKey);
+    console.log("log scaleType", scaleType);
+    const data = `
+  tabstave notation=true key=A time=4/4
 
-    // // Create VexFlow Renderer from canvas element with id #boo
-    // const renderer = new Renderer(outputNode, Renderer.Backends.SVG);
+  notes :q =|: (5/2.5/3.7/4) :8 7-5h6/3 ^3^ 5h6-7/5 ^3^ :q 7V/4 |
+  notes :8 t12p7/4 s5s3/4 :8 3s:16:5-7/5 :q p5/4
+  text :w, |#segno, ,|, :hd, , #tr
+`;
 
-    // // Initialize VexTab artist and parser.
-    // const artist = new Artist(10, 10, 600, { scale: 0.8 });
-    // const tab = new VexTab(artist);
-
-    // try {
-    //   tab.parse("tabstave").val();
-    //   artist.render(renderer);
-    // } catch (e) {
-    //   console.error(e);
-    // }
-
-    return () => {
-      if (outputNode) {
-        outputNode.textContent = "";
-      }
-    };
-  }, []);
-
-  return <div id="output"></div>;
+    // console.log(window.location.search = `tab=${data.replaceAll(' ', 'X')}`);
+  }, [scaleKey, scaleType]);
+  return (
+    <iframe
+      key={`${scaleKey}-${scaleType}`}
+      id="output"
+      width="100%"
+      height="300"
+      src="//jsfiddle.net/Digald/vmqnj4u6/40/embedded/result/"
+      allowFullScreen={true}
+      frameBorder="0"
+    ></iframe>
+  );
 };
 
 export default Scale;
