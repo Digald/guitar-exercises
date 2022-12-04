9d79833d1d9976471638688325c3516ba478069a
Mark Alaniz <markalaniz000@gmail.com>
2022-12-03 20:23:18 -0600

    complete asc and desc alternating exercise

--- components/utils/exercises.ts
+++ components/utils/exercises.ts
@@ -99,3 +99,65 @@ export const ascAndDesc = (
   });
   return data;
 };
+
+// TODO can this be combined with ascOrDesc?
+// TODO missing a repeat of the first position
+export const ascAndDescAlternating = (
+  scaleKey: string,
+  keyMaps: KeyMaps,
+  positions: Positions
+): string => {
+  let data = ""; // holds full display of staffs
+  let fullStaffLine = ""; // holds all of the lines for a single staff
+  let tempSwitch = false; // controls when to display a new staff line
+  const doubleKeyMaps = [
+    ...keyMaps[scaleKey as keyof typeof keyMaps],
+    ...keyMaps[scaleKey as keyof typeof keyMaps].slice(0, -1).reverse(),
+  ];
+
+  // map through each position in a key
+  doubleKeyMaps.forEach((position, index) => {
+    let newStaffLine = `tabstave notation=true key=${scaleKey} time=18/8`;
+
+    // calculate each note of each position in the scaleKey
+    const notesOfThisPosition = positions[
+      position.position as keyof typeof positions
+    ].map((note) => {
+      const splitNoteString = note.split("/");
+      const calculatedFret = (
+        Number(splitNoteString[0]) + position?.startingFret
+      ).toString();
+      splitNoteString[0] = calculatedFret;
+      return splitNoteString.join("/");
+    });
+
+    if (index < 7 && index % 2) {
+      // If the position is below the first seven and is even
+      notesOfThisPosition.reverse();
+    } else if (index >= 7 && index % 2) {
+      // If the position is the next set of seven and is odd
+      notesOfThisPosition.reverse();
+    }
+
+    // Set up scale for each position
+    let notesInMeasure = "\nnotes :8 ";
+    notesOfThisPosition.map((note) => {
+      notesInMeasure += `${note} `;
+    });
+    notesInMeasure += "|";
+
+    newStaffLine += notesInMeasure;
+
+    if (!tempSwitch) {
+      fullStaffLine += newStaffLine;
+      tempSwitch = true;
+      return;
+    }
+
+    fullStaffLine += notesInMeasure;
+    tempSwitch = false;
+    data += `\n${fullStaffLine}`;
+    fullStaffLine = "";
+  });
+  return data;
+};
